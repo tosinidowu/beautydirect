c8d29b658d48ba26e30c0f331b3082a5285c7253
tosinidowu <ttosinidowu@gmail.com>
2023-06-01 10:56:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Auth from './pages/Auth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="page-container">
+      <div className = "nav-container">
+      <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />        
+        <Route path="/login" element={<Auth />} />
+      </Routes>
+      </div>
+    </BrowserRouter>
+  
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+//import { useAuth } from '../Contexts/AuthContext';
+
+function Home(){
+    <h1>Hey hey hey</h1>
+}
+export default Home;
