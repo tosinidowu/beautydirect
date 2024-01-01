cc860cb4b52a59f2dd73bef98e14008ebc1ff68e
Bereket Sega <72763770+bereketsega@users.noreply.github.com>
2024-01-01 15:00:19 -0500

    client bug fixed

--- client/src/webpack.config.js
+++ client/src/webpack.config.js
@@ -1,10 +1,16 @@
 module.exports = {
     resolve: {
         fallback: {
-          "http": require.resolve("stream-http"),
-          "crypto": require.resolve("crypto-browserify"),
-          "zlib": require.resolve('browserify-zlib'),
-
-        }
-      }
-}
+            http: require.resolve("stream-http"),
+            crypto: false,
+            zlib: require.resolve("browserify-zlib"),
+            url: require.resolve("url/"),
+            querystring: require.resolve("querystring-es3"),
+            path: require.resolve("path-browserify"),
+            stream: require.resolve("stream-browserify"),
+            buffer: require.resolve("buffer/"),
+            util: require.resolve("util/"),
+            fs: false,
+        },
+    },
+};
