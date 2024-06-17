ea5995458035c464dc3836e2dcfc9e62250564fb
tosinidowu <ttosinidowu@gmail.com>
2024-06-16 23:55:38 -0400

    fix review route

--- server/controllers/review.controller.js
+++ server/controllers/review.controller.js
@@ -9,4 +9,37 @@ export class ReviewController {
         });
         res.status(201).json({ review: response });
     }
+
+    static async getReviews(req, res) {
+        const response = await ReviewModel.find({ servicerId: req.params.servicerId });
+        res.status(200).json({ reviews: response });
+    }
+
+    static async getSingleReview(req, res) {
+        const response = await ReviewModel.findOne({
+            userId: req.params.userId,
+            servicerId: req.params.servicerId,
+        });
+        res.status(200).json({ review: response });
+    }
+
+    static async updateReview(req, res) {
+        const response = await ReviewModel.findOneAndUpdate(
+            {
+                userId: req.params.userId,
+                servicerId: req.params.servicerId,
+            },
+            req.body,
+            { new: true }
+        );
+        res.status(200).json({ review: response });
+    }
+
+    static async deleteReview(req, res) {
+        const response = await ReviewModel.findOneAndDelete({
+            userId: req.params.userId,
+            servicerId: req.params.servicerId,
+        });
+        res.status(200).json({ review: response });
+    }
 }
